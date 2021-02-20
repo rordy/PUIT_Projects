62c30064898d44b73a0a6ec85d8303a27e38c3cd
Aleksandar Stefanovic <a.stefanovic2112@gmail.com>
2021-02-21 00:46:55 +0100

    Second Example on Multi-Threading - Done
    comments will be added later

---
+++ TEST/multithreading_II.py
@@ -0,0 +1,28 @@
+import threading
+import time
+
+class myThread(threading.Thread):
+    def __init__(self, threadID, name, delay):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.delay = delay
+    def run(self):
+        print("Starting " + self.name)
+        print_time(self.name, self.delay, 5)
+        print("Exiting " + self.name)
+
+def print_time(threadName, delay, counter):
+    while counter:
+        time.sleep(delay)
+        print("%s: %s" % (threadName, time.ctime(time.time())))
+        counter -= 1
+
+# Create new threads
+thread1 = myThread(1, "Thread-1", 1)
+thread2 = myThread(2, "Thread-2", 2)
+
+# Starting threads
+thread1.start()
+thread2.start()
+print("Main Thread")
