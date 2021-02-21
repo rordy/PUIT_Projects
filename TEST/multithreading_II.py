64a7bd9cad5a32c3586b4444f29196a484a935a5
Aleksandar Stefanovic <a.stefanovic2112@gmail.com>
2021-02-22 00:15:52 +0100

    Added comments on the code

--- TEST/multithreading_II.py
+++ TEST/multithreading_II.py
@@ -1,17 +1,27 @@
+# This is similar example ase miltithreading_I,
+# But in this case we create class and then through the class we pass the params
+
+# Importing necessary libraries
 import threading
 import time
 
+# Creating the class
 class myThread(threading.Thread):
+    # Defining class constructor
     def __init__(self, threadID, name, delay):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.delay = delay
+
+    # defining function run
     def run(self):
         print("Starting " + self.name)
+        # calling function 'print_time'
         print_time(self.name, self.delay, 5)
         print("Exiting " + self.name)
 
+# function print_time with all the necessary params
 def print_time(threadName, delay, counter):
     while counter:
         time.sleep(delay)
